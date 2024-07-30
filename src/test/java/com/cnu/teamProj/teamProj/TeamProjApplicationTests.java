530efefb52dde2ae10e1d143df81f4bb5d7cc0c9
Seung-0208 <esybd02@naver.com>
2024-07-31 08:22:41 +0900

    first upload

---
+++ src/test/java/com/cnu/teamProj/teamProj/TeamProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnu.teamProj.teamProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
