2337953ba5c30dea6c7685104564f9a579efa2fe
Seung-0208 <esybd02@naver.com>
2025-02-05 20:25:59 +0900

    feat: 파일 업로드 api 구현

--- src/main/java/com/cnu/teamProj/teamProj/config/S3Config.java
+++ src/main/java/com/cnu/teamProj/teamProj/config/S3Config.java
@@ -1,20 +1,33 @@
-//package com.cnu.teamProj.teamProj.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.time.Duration;
-//
-//@Configuration
-//public class S3Config {
-//    @Value("${spring.cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//    @Value("${spring.cloud.aws.credentials.secret-key}")
-//    private String accessSecret;
-//    @Value("${spring.cloud.aws.region.static}")
-//    private String region;
-//
-//    @Bean
-//    public Ama
-//}
+package com.cnu.teamProj.teamProj.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class S3Config {
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String accessSecret;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        //접근 키와 시크릿 키를 통해 자격증명 생성 = awsCredentials
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, accessSecret);
+        //위에서 생성한 자격증명을 바탕으로 client 객체 생성
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
