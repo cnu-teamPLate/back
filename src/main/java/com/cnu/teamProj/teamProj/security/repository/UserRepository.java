a7b4b5474af5ab80fbb649968cc47580af986925
Seung-0208 <esybd02@naver.com>
2025-03-22 18:09:38 +0900

    test: workflow 추가

--- src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.cnu.teamProj.teamProj.security.repository;
 
 import com.cnu.teamProj.teamProj.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,10 @@ public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByName(String name);
 
     Boolean existsByMail(String mail);
+
+    @Query("select user from User user where user.id like :query")
+    List<User> findUsersById(@Param("query") String query);
+
+    @Query("select user from User user where user.name like :query")
+    List<User> findUsersByName(@Param("query") String query);
 }
