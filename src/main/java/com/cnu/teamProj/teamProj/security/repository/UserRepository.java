a2280443b26ab24052fb1972785a45c0fdd061c7
Seung-0208 <esybd02@naver.com>
2025-03-22 18:16:51 +0900

    test: workflow 테스트

--- src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByMail(String mail);
+    Optional<User> findByMail(String mail); //test
     Boolean existsByName(String name);
 
     Boolean existsByMail(String mail);
