eac4b56dec3b43ed1d94d64cafb774bd58a5677d
Seung-0208 <esybd02@naver.com>
2025-03-22 23:29:13 +0900

    test: 워크플로우 테스트

--- src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByMail(String mail); //test test test test test test test test test test
+    Optional<User> findByMail(String mail); //test test test test test test test test test test test
     Boolean existsByName(String name);
 
     Boolean existsByMail(String mail);
