9a52ff4f9819b02c52069b95c6d9569f0c3aa363
Seung-0208 <esybd02@naver.com>
2025-03-23 23:41:20 +0900

    remove: .env 파일 추적 제거

--- src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
@@ -2,11 +2,8 @@ package com.cnu.teamProj.teamProj.security.repository;
 
 import com.cnu.teamProj.teamProj.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,10 +12,4 @@ public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByName(String name);
 
     Boolean existsByMail(String mail);
-
-    @Query("select user from User user where user.id like :query")
-    List<User> findUsersById(@Param("query") String query);
-
-    @Query("select user from User user where user.name like :query")
-    List<User> findUsersByName(@Param("query") String query);
 }
