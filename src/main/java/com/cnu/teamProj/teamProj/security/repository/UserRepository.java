bf64282d6ebb8360b926447dea2d76b4e8457624
Ahnnyong <itiscool7@naver.com>
2025-04-03 18:33:50 +0900

    feat: comment 추가

--- src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.cnu.teamProj.teamProj.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByName(String name);
 
     Boolean existsByMail(String mail);
+
+    List<User> findUsersById(String query);
+
+    List<User> findUsersByName(String query);
 }
