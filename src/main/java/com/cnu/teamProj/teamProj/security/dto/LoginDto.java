1161dddd4ec6bd19c6b801c94232276fd46149bd
Seung-0208 <esybd02@naver.com>
2025-07-17 19:13:55 +0900

    feat: 비밀번호 수정 api 추가

--- src/main/java/com/cnu/teamProj/teamProj/security/dto/LoginDto.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/dto/LoginDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "로그인 및 비밀번호 변경 시 사용되는 dto")
 public class LoginDto {
     @Schema(description = "학번", example = "20211079")
     private String id;
