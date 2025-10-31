aa8cf7535e92d6b6712070f1c72321bf744c4835
Seung-0208 <esybd02@naver.com>
2025-11-01 02:18:19 +0900

    chore: 워크플로우 수정

--- src/main/java/com/cnu/teamProj/teamProj/security/SecurityConstants.java
+++ src/main/java/com/cnu/teamProj/teamProj/security/SecurityConstants.java
@@ -1,17 +1,30 @@
 package com.cnu.teamProj.teamProj.security;
 
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.List;
 
+@Component
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;//
+    public static final long JWT_EXPIRATION = 70000;
 
-    @Value("${jwt-secret}")
     public static String JWT_SECRET;
-    public static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    public static SecretKey SECRET_KEY;
+
+    @Autowired
+    public SecurityConstants(Environment env) {
+        JWT_SECRET = env.getProperty("jwt-secret");
+        SECRET_KEY = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    }
+
+//    @Value("${jwt-secret}")
+//    public String JWT_SECRET;
+//    public static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
     public static final List<String> PUBLIC_ENDPOINTS = List.of( //모든 사용자가 접근 가능한 url
             "/auth/login",
             "/auth/register",
